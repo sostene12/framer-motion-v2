eb35ef94fdd1bf41e0da9b9518f56301eca8fa3a
sostene <ngarukiyimanasostene@gmail.com>
2022-04-09 10:30:30 +0200

    Updates

---
+++ precache-manifest.8a118621e7d8ffd61ae5a62b837bc4b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73f606d6dfcc63ca680e7b869d37ceb4",
+    "url": "/framer-motion-v2/index.html"
+  },
+  {
+    "revision": "60b3ae12d8848e3ed9b1",
+    "url": "/framer-motion-v2/static/css/main.86dbead9.chunk.css"
+  },
+  {
+    "revision": "47ce0c75ae6eb5a769cb",
+    "url": "/framer-motion-v2/static/js/2.88c56bf4.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/framer-motion-v2/static/js/2.88c56bf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60b3ae12d8848e3ed9b1",
+    "url": "/framer-motion-v2/static/js/main.481a3053.chunk.js"
+  },
+  {
+    "revision": "1d5d8559ac7a21f3fb75",
+    "url": "/framer-motion-v2/static/js/runtime-main.b988972a.js"
+  }
+]);
