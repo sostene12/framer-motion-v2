e0c3b2173961f7bc610ea7d14749c6756cbe9a1f
sostene <ngarukiyimanasostene@gmail.com>
2022-04-11 09:42:52 +0200

    created loading component and animate it

---
+++ src/components/Loader.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const loaderVariants = {
+  animate: {
+    x: [-20, 20],
+    y: [0, -30],
+    transition: {
+      x: {
+        yoyo: Infinity,
+        duration: 0.5,
+      },
+      y: {
+        yoyo: Infinity,
+        duration: 0.25,
+        ease: "easeOut",
+      },
+    },
+  },
+};
+
+const Loader = () => {
+  return (
+    <>
+      <motion.div
+        className="loader"
+        variants={loaderVariants}
+        animate="animate"
+      ></motion.div>
+    </>
+  );
+};
+
+export default Loader;
